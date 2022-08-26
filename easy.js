edbfe6fce20fd99740df2fe048fb35af0cdccd58
unknown <mhz.edu@gmail.com>
2022-08-26 12:17:44 +0300

    Problems 21, 26, 27

--- easy.js
+++ easy.js
@@ -5,43 +5,49 @@
  * @return {number[]}
  */
 // Bruteforce variant
- var twoSumBrute = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
+var twoSumBrute = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
+  }
 };
 // Recursive variant
-var twoSum = function(nums, target) {
-    let indexedArr = nums.map((item, index) => [index, item]);
-    
-    let mergeCall = (left, right, target) => {
-        for (let i = 0; i < left.length; i++) {
-            for (let j = 0; j < right.length; j++) {
-                if (left[i][1] + right[j][1] === target) {
-                    return [left[i][0], right[j][0]];
-                }
-            }
-        }
-    }
+var twoSum = function (nums, target) {
+  let indexedArr = nums.map((item, index) => [index, item]);
 
-    let recursiveCall = (nums, target) => {
-        if (nums.length === 1) { return; }
-        if (nums.length === 2) {
-            if (nums[0][1] + nums[1][1] === target) {
-                return [nums[0][0], nums[1][0]];
-            }
+  let mergeCall = (left, right, target) => {
+    for (let i = 0; i < left.length; i++) {
+      for (let j = 0; j < right.length; j++) {
+        if (left[i][1] + right[j][1] === target) {
+          return [left[i][0], right[j][0]];
         }
-        let mid = Math.round(nums.length / 2);
-        let leftHalf = nums.slice(0, mid);
-        let rightHalf = nums.slice(mid);
-        return recursiveCall(leftHalf, target) || recursiveCall(rightHalf, target) || mergeCall(leftHalf, rightHalf, target);
+      }
     }
+  };
 
-    return recursiveCall(indexedArr, target);
+  let recursiveCall = (nums, target) => {
+    if (nums.length === 1) {
+      return;
+    }
+    if (nums.length === 2) {
+      if (nums[0][1] + nums[1][1] === target) {
+        return [nums[0][0], nums[1][0]];
+      }
+    }
+    let mid = Math.round(nums.length / 2);
+    let leftHalf = nums.slice(0, mid);
+    let rightHalf = nums.slice(mid);
+    return (
+      recursiveCall(leftHalf, target) ||
+      recursiveCall(rightHalf, target) ||
+      mergeCall(leftHalf, rightHalf, target)
+    );
+  };
+
+  return recursiveCall(indexedArr, target);
 };
 
 /**
@@ -49,38 +55,37 @@ var twoSum = function(nums, target) {
  * @param {number} x
  * @return {boolean}
  */
- var isPalindromeStr = function(x) {
-    const stringRepresentation = Number(x).toString();
-    let i = 0;
-    let j = stringRepresentation.length - 1;
-    for (;i < (stringRepresentation.length / 2); i++ ) {
-        if (stringRepresentation[i] !== stringRepresentation[j - i]) {
-            return false;
-        }
-        return true;
-    };
+var isPalindromeStr = function (x) {
+  const stringRepresentation = Number(x).toString();
+  let i = 0;
+  let j = stringRepresentation.length - 1;
+  for (; i < stringRepresentation.length / 2; i++) {
+    if (stringRepresentation[i] !== stringRepresentation[j - i]) {
+      return false;
+    }
+    return true;
+  }
 };
 
 // No conversion to string
-var isPalindrome = function(x) {
-    let recCall = (num, digits) => {
-        if (digits < 2) {
-            return true;
-        } else {
-            let rightDigit = num % 10;
-            
-            let divider = Math.pow(10, digits - 1);
-            let leftDigit = Math.trunc(num / divider);
+var isPalindrome = function (x) {
+  let recCall = (num, digits) => {
+    if (digits < 2) {
+      return true;
+    } else {
+      let rightDigit = num % 10;
 
-            let remainder = Math.trunc((num  % divider) / 10);
-        
-            return (leftDigit === rightDigit) && recCall(remainder, digits - 2);
-        }
+      let divider = Math.pow(10, digits - 1);
+      let leftDigit = Math.trunc(num / divider);
+
+      let remainder = Math.trunc((num % divider) / 10);
+
+      return leftDigit === rightDigit && recCall(remainder, digits - 2);
     }
+  };
 
-    let digitsNumber  = Math.trunc(Math.log10(x)) + 1;
-    return recCall(x, digitsNumber);
-
+  let digitsNumber = Math.trunc(Math.log10(x)) + 1;
+  return recCall(x, digitsNumber);
 };
 
 /**
@@ -88,72 +93,72 @@ var isPalindrome = function(x) {
  * @param {string} s
  * @return {number}
  */
- var romanToInt = function(s) {
-    switch (s[0]) {
-        case 'I':
-            switch (s.slice(0, 2)) {
-                case 'IV':
-                    return 4 + romanToInt(s.slice(2));
-                    break;
-                case 'IX':
-                    return 9 + romanToInt(s.slice(2));
-                    break;
-                case 'I':
-                    return 1;
-                    break
-                default:
-                    return 1 + romanToInt(s.slice(1));
-                    break;
-            }
-            break;
-        case 'V':
-            return 5 + romanToInt(s.slice(1));
-            break;
-        case 'X':
-            switch (s.slice(0, 2)) {
-                case 'XL':
-                    return 40 + romanToInt(s.slice(2));
-                    break;
-                case 'XC':
-                    return 90 + romanToInt(s.slice(2));
-                    break;
-                case 'X':
-                    return 10;
-                    break
-                default:
-                    return 10 + romanToInt(s.slice(1));
-                    break;
-            }
-            break;
-        case 'L':
-            return 50 + romanToInt(s.slice(1));
-            break;
-        case 'C':
-            switch (s.slice(0, 2)) {
-                case 'CD':
-                    return 400 + romanToInt(s.slice(2));
-                    break;
-                case 'CM':
-                    return 900 + romanToInt(s.slice(2));
-                    break;
-                case 'C':
-                    return 100;
-                    break
-                default:
-                    return 100 + romanToInt(s.slice(1));
-                    break;
-            }
-            break;
-        case 'D':
-            return 500 + romanToInt(s.slice(1));
-            break;
-        case 'M':
-            return 1000 + romanToInt(s.slice(1));
-            break;
+var romanToInt = function (s) {
+  switch (s[0]) {
+    case "I":
+      switch (s.slice(0, 2)) {
+        case "IV":
+          return 4 + romanToInt(s.slice(2));
+          break;
+        case "IX":
+          return 9 + romanToInt(s.slice(2));
+          break;
+        case "I":
+          return 1;
+          break;
         default:
-            return 0;
-            break;
-    }
+          return 1 + romanToInt(s.slice(1));
+          break;
+      }
+      break;
+    case "V":
+      return 5 + romanToInt(s.slice(1));
+      break;
+    case "X":
+      switch (s.slice(0, 2)) {
+        case "XL":
+          return 40 + romanToInt(s.slice(2));
+          break;
+        case "XC":
+          return 90 + romanToInt(s.slice(2));
+          break;
+        case "X":
+          return 10;
+          break;
+        default:
+          return 10 + romanToInt(s.slice(1));
+          break;
+      }
+      break;
+    case "L":
+      return 50 + romanToInt(s.slice(1));
+      break;
+    case "C":
+      switch (s.slice(0, 2)) {
+        case "CD":
+          return 400 + romanToInt(s.slice(2));
+          break;
+        case "CM":
+          return 900 + romanToInt(s.slice(2));
+          break;
+        case "C":
+          return 100;
+          break;
+        default:
+          return 100 + romanToInt(s.slice(1));
+          break;
+      }
+      break;
+    case "D":
+      return 500 + romanToInt(s.slice(1));
+      break;
+    case "M":
+      return 1000 + romanToInt(s.slice(1));
+      break;
+    default:
+      return 0;
+      break;
+  }
 };
 
 /**
@@ -161,109 +166,195 @@ var isPalindrome = function(x) {
  * @param {string[]} strs
  * @return {string}
  */
- var longestCommonPrefix1 = function(strs) {
-    let chars = [];
-    let strings = [];
-    for (const elem of strs) {
-        chars.push(elem.slice(0, 1));
-        strings.push(elem.slice(1));
-    }
-    if (chars.every(item => item === chars[0])) {
-        if (chars[0] === '') {
-            return '';
-        } else {
-            return chars[0] + longestCommonPrefix(strings);
-        }
+var longestCommonPrefix1 = function (strs) {
+  let chars = [];
+  let strings = [];
+  for (const elem of strs) {
+    chars.push(elem.slice(0, 1));
+    strings.push(elem.slice(1));
+  }
+  if (chars.every((item) => item === chars[0])) {
+    if (chars[0] === "") {
+      return "";
     } else {
-        return '';
+      return chars[0] + longestCommonPrefix(strings);
     }
+  } else {
+    return "";
+  }
 };
 // Varian with one cycle
-var longestCommonPrefix2 = function(strs) {
-    let recCall = function(pos) {
-        let char = strs[0][pos];
-        if (!char) {
-            return '';
-        }
-        for (const elem of strs) {
-            if (char === elem[pos]) {
-                continue;
-            } else {
-                return '';
-            }
-        }
-        return char + recCall(pos + 1);
-    };
-    return recCall(0);
+var longestCommonPrefix2 = function (strs) {
+  let recCall = function (pos) {
+    let char = strs[0][pos];
+    if (!char) {
+      return "";
+    }
+    for (const elem of strs) {
+      if (char === elem[pos]) {
+        continue;
+      } else {
+        return "";
+      }
+    }
+    return char + recCall(pos + 1);
+  };
+  return recCall(0);
 };
 // Devide and conquer approach
-var longestCommonPrefix = function(strs) {
-    let lcr2 = function(strings) {
-        let min, max;
-        if (strings[0].length >= strings[1].length) {
-            min = strings[0];
-            max = strings[1];
-        } else {
-            min = strings[1];
-            max = strings[0];
-        }
-        while(!max.startsWith(min) && min.length > 0) {
-            min = min.slice(0, -1)
-        };
-        return min;
-    };
-    let recCall = function(strings) {
-        if (strings.length === 0) {
-            return '';
-        }
-        if (strings.length === 1) {
-            return strings[0];
-        }
-        if (strings.length === 2) {
-            return lcr2(strings);
-        }
-        let mid = Math.round(strings.length / 2);
-        let leftLCR = recCall(strings.slice(0, mid));
-        let rightLCR = recCall(strings.slice(mid));
-        return lcr2([leftLCR, rightLCR]);
-    };
-    return recCall(strs);
-}
+var longestCommonPrefix = function (strs) {
+  let lcr2 = function (strings) {
+    let min, max;
+    if (strings[0].length >= strings[1].length) {
+      min = strings[0];
+      max = strings[1];
+    } else {
+      min = strings[1];
+      max = strings[0];
+    }
+    while (!max.startsWith(min) && min.length > 0) {
+      min = min.slice(0, -1);
+    }
+    return min;
+  };
+  let recCall = function (strings) {
+    if (strings.length === 0) {
+      return "";
+    }
+    if (strings.length === 1) {
+      return strings[0];
+    }
+    if (strings.length === 2) {
+      return lcr2(strings);
+    }
+    let mid = Math.round(strings.length / 2);
+    let leftLCR = recCall(strings.slice(0, mid));
+    let rightLCR = recCall(strings.slice(mid));
+    return lcr2([leftLCR, rightLCR]);
+  };
+  return recCall(strs);
+};
 
 /**
  * 20. Valid Parentheses
  * @param {string} s
  * @return {boolean}
  */
- var isValid = function(s) {
-    if (s.length === 1) {
-        return false;
-    }
-    let stack = [];
-    for (let i = 0; i < s.length; i++) {
-        switch (s[i]) {
-            case '(':
-            case '[':
-            case '{':
-                stack.push(s[i]);
-                break;
-            case ')':
-            case ']':
-            case '}':
-                if (stack.length === 0) {
-                    return false;
-                }
-                let stackTop = stack.pop();
-                if (s[i].charCodeAt(0) === stackTop.charCodeAt(0) + 1 || s[i].charCodeAt(0) === stackTop.charCodeAt(0) + 2) {
-                    continue;
-                } else {
-                    return false;
-                }
-                break;
-            default:
-                return false;
-                break;
+var isValid = function (s) {
+  if (s.length === 1) {
+    return false;
+  }
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
+      case "(":
+      case "[":
+      case "{":
+        stack.push(s[i]);
+        break;
+      case ")":
+      case "]":
+      case "}":
+        if (stack.length === 0) {
+          return false;
         }
+        let stackTop = stack.pop();
+        if (
+          s[i].charCodeAt(0) === stackTop.charCodeAt(0) + 1 ||
+          s[i].charCodeAt(0) === stackTop.charCodeAt(0) + 2
+        ) {
+          continue;
+        } else {
+          return false;
+        }
+        break;
+      default:
+        return false;
+        break;
     }
-    return (stack.length === 0);
+  }
+  return stack.length === 0;
+};
+
+/**
+ * 21. Merge two sorted lists
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  if (!list1 || !list1.hasOwnProperty("val")) {
+    return list2;
+  } else if (!list2 || !list2.hasOwnProperty("val")) {
+    return list1;
+  } else {
+    return list1.val > list2.val
+      ? {
+          val: list2.val,
+          next: mergeTwoLists(list1, list2.next),
+        }
+      : {
+          val: list1.val,
+          next: mergeTwoLists(list1.next, list2),
+        };
+  }
+};
+
+/**
+ * 26. Remove Duplicates from Sorted Array
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let base = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[base] !== nums[i]) {
+      nums[base + 1] = nums[i];
+      base += 1;
+    }
+  }
+  return base + 1;
+};
+
+/**
+ * 27. Remove Element
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let currentElementPointer = 0;
+  let lastElementPointer = nums.length - 1;
+  while (currentElementPointer < nums.length) {
+    console.log(currentElementPointer, lastElementPointer, nums);
+    if (currentElementPointer == lastElementPointer) {
+      if (nums[currentElementPointer] === val) {
+        lastElementPointer -= 1;
+        break;
+      } else {
+        break;
+      }
+    } else if (
+      nums[currentElementPointer] === val &&
+      nums[lastElementPointer] !== val
+    ) {
+      nums[currentElementPointer] = nums[lastElementPointer];
+      lastElementPointer -= 1;
+    } else if (
+      nums[currentElementPointer] === val &&
+      nums[lastElementPointer] === val
+    ) {
+      lastElementPointer -= 1;
+    } else {
+      currentElementPointer += 1;
+    }
+  }
+  return lastElementPointer + 1;
 };
--- test.js
+++ test.js
@@ -122,3 +122,123 @@ describe("20. Valid Parentheses", function() {
         assert.isFalse(isValid(')('));
     });
 });
+
+describe("21. Merge two sorted lists", function() {
+    it("merge two lists that are sequntial", function() {
+        const list1 = {val:1,next:{val:2,next:{val:3}}}
+        const list2 = {val:4,next:{val:5,next:{val:6}}}
+        const mergedList = {val:1,next:{val:2,next:{val:3,next:{val:4,next:{val:5,next:{val:6}}}}}}
+        assert.deepEqual(mergeTwoLists(list1,list2), mergedList)
+    });
+    it("merge two lists that are not sequntial", function() {
+        const list1 = {val:2,next:{val:4,next:{val:6}}}
+        const list2 = {val:3,next:{val:5,next:{val:7}}}
+        const mergedList = {val:2,next:{val:3,next:{val:4,next:{val:5,next:{val:6,next:{val:7}}}}}}
+        assert.deepEqual(mergeTwoLists(list1,list2), mergedList)
+    });
+    it("merge two lists, when one list is empty", function() {
+        const list1 = {val:2,next:{val:4,next:{val:6}}}
+        const list2 = {}
+        const mergedList = {val:2,next:{val:4,next:{val:6}}}
+        assert.deepEqual(mergeTwoLists(list1,list2), mergedList)
+    });
+    it("both lists are empty", function() {
+        const list1 = {}
+        const list2 = {}
+        const mergedList = {}
+        assert.deepEqual(mergeTwoLists(list1,list2), mergedList)
+    });
+    it("both lists have one element", function() {
+        const list1 = {val: 50}
+        const list2 = {val: 34}
+        const mergedList = {val: 34, next: {val:50}}
+        assert.deepEqual(mergeTwoLists(list1,list2), mergedList)
+    });
+    it("One list has one element to insert in the middle of another", function() {
+        const list1 = {val:2,next:{val:4,next:{val:6, next:{val:8}}}}
+        const list2 = {val: 5}
+        const mergedList = {val:2,next:{val:4,next:{val:5,next:{val:6, next:{val:8}}}}}
+        assert.deepEqual(mergeTwoLists(list1,list2), mergedList)
+    });
+})
+
+describe('26. Remove Duplicates from Sorted Array', function() {
+    it('short array, duplicates at the begining', function() {
+        const nums = [1,1,2]
+        const outNums = [1,2]
+        let k = removeDuplicates(nums)
+        assert.deepEqual(nums.slice(0,k), outNums)
+    })
+    it('long array, duplicates at the beginig', function() {
+        const nums = [0,0,1,1,1,2,2,3,3,4]
+        const outNums = [0,1,2,3,4]
+        let k = removeDuplicates(nums)
+        assert.deepEqual(nums.slice(0,k), outNums)
+    })
+    it('long array, duplicates at the end', function() {
+        const nums = [0,1,2,3,4,4,5,5,5,6,6]
+        const outNums = [0,1,2,3,4,5,6]
+        let k = removeDuplicates(nums)
+        assert.deepEqual(nums.slice(0,k), outNums)
+    })
+    it('long array, duplicates int the middle', function() {
+        const nums = [0,1,2,3,4,4,5,6,7]
+        const outNums = [0,1,2,3,4,5,6,7]
+        let k = removeDuplicates(nums)
+        assert.deepEqual(nums.slice(0,k), outNums)
+    })
+    it('no duplicates', function() {
+        const nums = [0,1,2,3,4]
+        const outNums = [0,1,2,3,4]
+        let k = removeDuplicates(nums)
+        assert.deepEqual(nums.slice(0,k), outNums)
+    })
+    it('empty array', function() {
+        const nums = []
+        const outNums = []
+        let k = removeDuplicates(nums)
+        assert.deepEqual(nums.slice(0,k), outNums)
+    })
+})
+
+describe('27. Remove Element', function() {
+        it('long array, remove elements at the beginig', function() {
+            const nums = [0,0,1,1,1,2,2,3,3,4]
+            const outNums = [4,3,1,1,1,2,2,3]
+            let k = removeElement(nums, 0)
+            assert.deepEqual(nums.slice(0,k), outNums)
+        })
+        it('long array, remove elements in the middle', function() {
+            const nums = [0,0,1,1,1,2,2,3,3,4]
+            const outNums = [0,0,4,3,3,2,2]
+            let k = removeElement(nums, 1)
+            assert.deepEqual(nums.slice(0,k), outNums)
+        })
+        it('long array, remove elements at the end', function() {
+            const nums = [0,0,1,1,1,2,2,3,3,4]
+            const outNums = [0,0,1,1,1,2,2,3,3]
+            let k = removeElement(nums, 4)
+            console.log(k, nums)
+            assert.deepEqual(nums.slice(0,k), outNums)
+        })
+        it('long array, remove elements scattered', function() {
+            const nums = [0,0,1,2,1,2,3,1,4]
+            const outNums = [0,0,4,2,3,2]
+            let k = removeElement(nums, 1)
+            console.log(k, nums)
+            assert.deepEqual(nums.slice(0,k), outNums)
+        })
+        it('val is not found', function() {
+            const nums = [0,0,1,1,1,2,2,3,3,4]
+            const outNums = [0,0,1,1,1,2,2,3,3,4]
+            let k = removeElement(nums, 5)
+            assert.deepEqual(nums.slice(0,k), outNums)
+        })
+        it('array is empty', function() {
+            const nums = []
+            const outNums = []
+            let k = removeElement(nums, 5)
+            assert.deepEqual(nums.slice(0,k), outNums)
+        })
+        
+})
